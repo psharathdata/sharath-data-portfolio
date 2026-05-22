2f0bf97d9bfe7be8e035373f7ab175cee501025f
psharathdata <p.sharath.data@gmail.com>
2026-05-22 15:51:27 +0530

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './style.css'
-import Portfolio from './Portfolio.jsx'
-
-createRoot(document.getElementById('root')).render(<Portfolio />)
