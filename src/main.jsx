d826bc347b11bb8f7d6e56439cbc90a88ec7d6a2
psharathdata <p.sharath.data@gmail.com>
2026-05-22 15:51:46 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './style.css'
+import Portfolio from './Portfolio.jsx'
+
+createRoot(document.getElementById('root')).render(<Portfolio />)
